0509884546b3f8723ab784082c85982f1580bdb5
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-10-30 10:47:12 +0300

    Tests are Doen

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, {useEffect, useState} from "react";
+
+function App (){
+    const [imgs, setImgs] = useState("");
+    const [loading , setLoading] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => {
+            setImgs(data.message);
+            setLoading(true);
+        });
+    }, []);
+
+    
+    if(!loading) {return <p>loading...</p>}
+
+
+    return (
+        <div>
+            <img src={imgs} alt="A Random Dog" />
+        </div>
+    )
+}
+
+export default App;
